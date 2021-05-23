a0f113350897c514711ff4a7b316f5b83b98193a
klind <klpcrap@gmail.com>
2021-05-23 16:29:15 -0700

    Cognitive Complexity of 11

--- src/main/java/com/code/logincache/LoginCache.java
+++ src/main/java/com/code/logincache/LoginCache.java
@@ -1,21 +1,21 @@
 package com.code.logincache;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * Cache implementation.
  */
 @Component
 public class LoginCache {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginCache.class);
     // The max cache size
     private static final int MAX_CACHE = 10;
@@ -28,44 +28,53 @@ public class LoginCache {
     private static Map<String, LocalDateTime> cache = new HashMap<>(MAX_CACHE);
 
     /**
-     * Adds the specified user id with the specified time to the cache.
-     * If the cache has reached its max size, the oldest entry will be removed.
+     * Adds the specified user id with the specified time to the cache. If the cache has reached its max size, the oldest entry will be removed.
      *
      * @param userId The user id.
-     * @param now current time.
+     * @param now    current time.
      */
     public final void addToCache(final String userId, final LocalDateTime now) {
-        if (StringUtils.isEmpty(userId) || now == null) {
+        if (StringUtils.isEmpty(userId)) {
             LOGGER.warn("Cannot add empty/null user id or null login time to the cache");
             return;
         }
         if (cache.size() == MAX_CACHE && !cache.containsKey(userId)) {
-            final String oldestUserId = lastLoggedInUserIds.remove();
-            LOGGER.debug("Removed {} from lastLoggedInUserIds", oldestUserId);
-            if (oldestUserId == null) {
-                // If the lastLoggedInUserIds list is empty clear the cache.
-                LOGGER.warn("Clearing the cache and lastLoggedInUserIds as {} was not found in lastLoggedInUserIds", oldestUserId);
-                cache.clear();
-                lastLoggedInUserIds.clear();
-            } else {
-                final LocalDateTime removedFromCache = cache.remove(oldestUserId);
-                if (removedFromCache == null) {
-                    // If the user id retrieved from lastLoggedInUserIds was not found on the cache clear the cache.
-                    LOGGER.warn("Clearing the cache and lastLoggedInUserIds as {} was not found in cache, but was found in lastLoggedInUserIds ", oldestUserId);
-                    cache.clear();
-                    lastLoggedInUserIds.clear();
-                } else {
-                    LOGGER.debug("Removed {} from cache", oldestUserId);
-                }
-            }
+            validateCache();
         }
         lastLoggedInUserIds.add(userId);
         cache.put(userId, now);
         LOGGER.debug("Added {} to the cache", userId);
     }
 
+    private void validateCache() {
+        if (lastLoggedInUserIds.isEmpty()) {
+            // If the lastLoggedInUserIds list is empty clear the cache.
+            LOGGER.warn("Clearing the cache as lastLoggedInUserIds is empty");
+            cache.clear();
+        } else {
+            final String oldestUserId = lastLoggedInUserIds.poll();
+            LOGGER.debug("Removed {} from lastLoggedInUserIds", oldestUserId);
+            final boolean removedFromCache = removeFromCache(oldestUserId);
+            if (!removedFromCache) {
+                // If the user id retrieved from lastLoggedInUserIds (oldestUserId) was not found on the cache, then clear the cache.
+                LOGGER.warn("Clearing the cache and lastLoggedInUserIds as {} was not found in cache, but was found in lastLoggedInUserIds ", oldestUserId);
+                cache.clear();
+                lastLoggedInUserIds.clear();
+            }
+        }
+    }
+
+    private boolean removeFromCache(String id) {
+        LocalDateTime removedEntry = cache.remove(id);
+        if (removedEntry != null) {
+            LOGGER.debug("Removed {} from cache", id);
+        }
+        return removedEntry != null;
+    }
+
     /**
      * Returns the current size of the cache.
+     *
      * @return int The size of the cache.
      */
     public final int getCacheSize() {
@@ -73,7 +82,6 @@ public class LoginCache {
     }
 
     /**
-     *
      * @param userId The user id.
      * @return boolean If cache contains the user id return true, otherwise false.
      */
@@ -83,6 +91,7 @@ public class LoginCache {
 
     /**
      * Returns the last login for the specified user id.
+     *
      * @param userId The user id.
      * @return LocalDateTime The last login time.
      */
